891a2116a4eb398c6acc036e8ced40d9efbe4f44
LucasGuiron <lucasguirondj@hotmail.com>
2024-01-16 19:15:35 -0300

    parte de add litorais 60%, falta fazer sumir e ficar clicavel novamente

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+// tela de login e cadastro
+
 function abrirModal(){
     const modal = document.querySelector("#janela-modal");
     modal.classList.add('abrir');
@@ -7,3 +9,50 @@ function abrirModal(){
             modal.classList.remove('abrir');
     }});
 };
+
+// Parte esquerda dos litorias 
+
+const cidades = {
+    cidade:{
+        sp:document.querySelector(".cidade"),
+    }
+};
+
+const litorias = [
+    "Litoral Norte",
+    "Litoral Sul",
+];
+
+function addLitoral(){
+    cidades.cidade.sp
+    
+    if(document.getElementsByClassName(cidades.cidade.sp) == document.getElementsByClassName(cidades.cidade.sp) ){
+
+        cidades.cidade.sp.classList.add('litorais');
+
+        criaLiLitoral();
+    }
+
+    cidades.cidade.sp.addEventListener("click" , removeLirotal)
+};
+
+function removeLirotal(){
+
+    cidades.cidade.sp.classList.remove('litorais');
+
+}
+
+ function criaLiLitoral(){
+
+     for(let i= 0; i < litorias.length; i++){
+         const itemLitoral = document.createElement("li");
+         itemLitoral.textContent = `${litorias[i]}`
+         const texLitoral = document.createTextNode("");
+
+         itemLitoral.appendChild(texLitoral);
+
+         document.querySelector(".litorais").appendChild(itemLitoral);
+     };
+   
+ };
+
