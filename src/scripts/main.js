faf6a22c68f1811f5d1177ad03a1277a02c2cbb4
LucasGuiron <lucasguirondj@hotmail.com>
2024-01-18 22:09:56 -0300

    correção, tem uns bug para arrumar para aparecer os litorais

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,62 +12,71 @@ function abrirModal(){
 
 // Parte esquerda dos litorias 
 
- const cidades = {
-     cidade:{
-         cidades: document.querySelector(".cidades"),
-         litoral:document.querySelector('.litorais'),
-     }
- };
-
- const capitais = [
-     "São Paulo",
-     "Rio de Janeira",
-     "Salvador",
-     "Fortaleza"
- ]
-
- const litorias = [
-     "Litoral Norte",
-     "Litoral Sul",
- ];
-
- let openlitoral = [];
-
- for(let i =0; i < capitais.length; i++){
-     let box= document.createElement("a");
-     box.className = "cidade";
-     box.innerHTML = capitais[i];
-     box.onclick = hanleClick;
-     document.querySelector(".cidades").appendChild(box);
- }
-
- function hanleClick() {
-     if(openlitoral.length < 2){
-         criaLiLitoral();
-         openlitoral.push(this);
-     }
-
-     if(capitais.length == 2){
-           removeCidade();
-     }
- }
-
-
- function criaLiLitoral(){
-
-     for(let i= 0; i < litorias.length; i++){
-         const itemLitoral = document.createElement("a");
-         itemLitoral.textContent = `${litorias[i]}`
-         const texLitoral = document.createTextNode("");
-
-         itemLitoral.appendChild(texLitoral);
-
-         document.querySelector(".litorais").appendChild(itemLitoral);
-     };
+const cidades = {
+    cidade: {
+      cidades: document.querySelector(".cidades"),
+      litoral: document.querySelector('.litorais'),
+    }
+  };
   
- };
+  const capitais = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Salvador",
+    "Fortaleza"
+  ];
+  
+  const litorias = [
+    "Litoral Norte",
+    "Litoral Sul",
+  ];
+  
+  let openlitoral = [];
+  
+  for (let i = 0; i < capitais.length; i++) {
+    let box = document.createElement("a");
+    box.className = "cidade";
+    box.innerHTML = capitais[i];
+    box.onclick = handleClick;
+    document.querySelector(".cidades").appendChild(box);
+  }
+  
+  function handleClick() {
+    const isSelected = openlitoral.includes(this);
+  
+    if (isSelected) {
+      // Cidade já selecionada, remover litorais
+      removeLitorais();
+      openlitoral = openlitoral.filter(city => city !== this);
+    } else {
+      // Cidade não selecionada, adicionar litorais
+      createLitorais();
+      openlitoral.push(this);
+    }
+  }
 
- function removeLirotal(){
-
-     cidades.cidade.litoral.classList.remove("litorais");
- };
+  function createLitorais() {
+    // Remova os litorais existentes antes de adicionar novos
+    removeLitorais();
+  
+    for (let i = 0; i < litorias.length; i++) {
+      const itemLitoral = document.createElement("a");
+      itemLitoral.textContent = `${litorias[i]}`;
+      document.querySelector(".litorais").appendChild(itemLitoral);
+    }
+  
+    // Adicione a classe "litorais" para torná-los visíveis
+    cidades.cidade.litoral.classList.add("litorais");
+  };
+  
+  function removeLitorais() {
+    // Remova a classe "litorais" para ocultar os litorais
+    cidades.cidade.litoral.classList.remove("litorais");
+  
+    // Remova todos os elementos filhos dos litorais
+    const litoraisContainer = document.querySelector(".litorais");
+    while (litoraisContainer.firstChild) {
+      litoraisContainer.removeChild(litoraisContainer.firstChild);
+    }
+  }
+  
